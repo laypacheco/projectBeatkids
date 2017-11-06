2e3376d382482d22fe9b39fb1bd120698f8430cf
laypacheco <layssapacheco@gmail.com>
2017-11-06 17:06:10 +0100

    First version

---
+++ proyectoEntero/Credentials.h
@@ -0,0 +1,18 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+#define CS         53 
+#define MOSI      51
+#define MISO      50 
+#define SCK        52
+// Wifi parameters
+//char passphrase[] = "yrkn7110";
+//char ssid[] = "AndroidAp";
+
+//char passphrase[] = "ooleizaiuuahng0O";
+//char ssid[] = " UDWIFI";
+
+char passphrase[] = "kirderbueno";
+char ssid[] = "linksys7";
+
+#endif
